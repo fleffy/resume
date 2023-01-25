358af560620b8645f601aac4489df080c86363e4
Yevhenii Sivukhin <fleffydesign@gmail.com>
2023-01-25 07:53:52 +0200

    some changes

--- src/components/SavePdfButton.jsx
+++ src/components/SavePdfButton.jsx
@@ -2,7 +2,7 @@ export const SavePdfButton = () => {
 	return (
 		<button
 			onClick={() => window.print()}
-			className='px-3 py-1 mb-2 rounded-md text-white font-semibold bg-slate-400 hover:bg-slate-700 transition-all mt-10'
+			className='px-3 py-1 mb-2 rounded-md text-white font-semibold bg-slate-400 hover:bg-slate-700 transition-all mt-10 invisible md:visible'
 		>
 			save pdf
 		</button>
